4eef693d7a9619feee28081718c5ec6b9ab3d2ba
theansweris4ty2 <105891937+theansweris4ty2@users.noreply.github.com>
2026-03-21 21:01:06 -0700

    Delete maps directory

--- maps/backgrounds.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="backgrounds" tilewidth="16" tileheight="16" tilecount="96" columns="12">
- <image source="../assets/TrapmoorTileset_v03.png" width="192" height="128"/>
-</tileset>
